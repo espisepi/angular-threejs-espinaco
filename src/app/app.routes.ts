0762201e7bc5fbfdbf4c550d93f10841b5ccaf9c
espisepi <sepinaco@icloud.com>
2023-11-18 17:22:20 +0100

    threejs component hello world

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ThreejsComponent } from './threejs/threejs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'threejs', component: ThreejsComponent },
+];
---
+++ src/app/threejs/threejs.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-threejs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './threejs.component.html',
+  styleUrl: './threejs.component.scss'
+})
+export class ThreejsComponent {
+
+}
